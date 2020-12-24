b5efc77ce587cd9f155edae1803093bbb33c9e94
anibalvf <33007157+anibalvf@users.noreply.github.com>
2020-12-24 07:29:36 +0100

    vamos que nos vamos

--- djangoweb/settings.py
+++ djangoweb/settings.py
@@ -122,3 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = ''
 
+STATICFILES_DIRS=(
+    'static',
+)
+
--- hookah/views.py
+++ hookah/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def firstview(request):
     context ={
-        'sample_var':"ejemplo"
+        'sample_var':"ejemplo",
+        'sample_dario':[1,4,5,6]
 
     }
     return render(request,'hookah.html',context)
