fed1e08af8ff063e3c8f3382d83e52269a1ea2a9
anibalvf <33007157+anibalvf@users.noreply.github.com>
2020-12-24 20:02:28 +0100

    si

--- djangoweb/urls.py
+++ djangoweb/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('hookah/', include('hookah.urls'))
+    path('hookah/', include('hookah.urls')),
+
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
